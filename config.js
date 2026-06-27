4dfbcd859d7c7a5bc190b45f96eaf564f1d6c28d
robusto-tech <robusto.tech@gmail.com>
2026-06-27 16:04:36 +0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwKneGyCE1G2wBmaMLEhc8gHEobqK2Jmsc85JTTiz4w0yaCfVirBoZqAsNYyoUGHl7gyg/exec',
-  MASTER_SHEET_ID: '1BRPYaRfQMihXcdeTAyzI559yI7muF1UDkei-03NdkJo',
-  MASTER_PASSWORD: 'admin007',
-};
